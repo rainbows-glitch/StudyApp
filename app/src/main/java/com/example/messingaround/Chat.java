f4284cbd04d619051bc04eb6982806a10382fe1c
Dashik <kumardashik@gmail.com>
2022-05-08 22:59:09 +1200

    navigation component added

---
+++ app/src/main/java/com/example/messingaround/Chat.java
@@ -0,0 +1,18 @@
+package com.example.messingaround;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Chat extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/messingaround/home.java
@@ -0,0 +1,18 @@
+package com.example.messingaround;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class home extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
