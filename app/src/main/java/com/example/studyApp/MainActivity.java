29efa551044adb1f730bcf0ffbf1d5a379198513
Dashik <kumardashik@gmail.com>
2022-05-31 23:36:05 +1200

    ActionBarDrawerToggle added

--- app/src/main/java/com/example/studyApp/MainActivity.java
+++ app/src/main/java/com/example/studyApp/MainActivity.java
@@ -1,27 +1,46 @@
 package com.example.studyApp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.Guideline;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    protected ImageView foreWave;
-    protected Guideline alphaGuide;
+    private ImageView foreWave;
+    private Guideline alphaGuide;
+    private Toolbar toolBar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar(findViewById(R.id.toolbar));
 
+        toolBar = findViewById(R.id.toolbar);
+        mDrawerLayout = findViewById(R.id.drawerLayout);
         foreWave = findViewById(R.id.foreWave);
         alphaGuide = findViewById(R.id.alphaGuide);
-        foreWave.post(() -> {
-            alphaGuide.setGuidelineEnd(foreWave.getHeight()/7);
-            Log.d("alphawave","guide adjusted");
-        });
+
+        setSupportActionBar(toolBar);
+        ActionBarDrawerToggle navToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(navToggle);
+        navToggle.syncState();
+
+        foreWave.post(() -> alphaGuide.setGuidelineEnd(foreWave.getHeight()/7));
     }
-}
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+        super.onBackPressed();
+    }
+}}
