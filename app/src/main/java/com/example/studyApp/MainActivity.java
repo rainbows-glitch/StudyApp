0575e3a2e0ec1b10de80ce08303517a8e6bd81aa
Dashik <kumardashik@gmail.com>
2022-06-02 23:55:20 +1200

    centred header textView

--- app/src/main/java/com/example/studyApp/MainActivity.java
+++ app/src/main/java/com/example/studyApp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.studyApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.constraintlayout.widget.Guideline;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -9,18 +11,19 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 //    declare variables
+    private ConstraintLayout toolBarLayout;
+    private ConstraintSet mConstraintSet;
+    private TextView header;
     private ImageView foreWave;
     private Guideline alphaGuide;
     private Toolbar toolBar;
-    private DrawerLayout mDrawerLayout;
-    private NavHostFragment navHost;
     private NavController mNavController;
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -30,16 +33,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        initialize variables
+        toolBarLayout = (ConstraintLayout) findViewById(R.id.toolBarConstraint);
         toolBar = findViewById(R.id.toolbar);
-        mDrawerLayout = findViewById(R.id.drawerLayout);
-        navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navhost);
+        header = findViewById(R.id.header);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        NavHostFragment navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navhost);
+        assert navHost != null;
         mNavController = navHost.getNavController();
 
         setSupportActionBar(toolBar);
 
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_study, R.id.nav_chat).setOpenableLayout(mDrawerLayout).build();
-        NavigationUI.setupWithNavController(toolBar, mNavController, mDrawerLayout);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_study, R.id.nav_chat).setOpenableLayout(drawerLayout).build();
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mAppBarConfiguration);
         NavigationUI.setupWithNavController((NavigationView) findViewById(R.id.drawer), mNavController);
+
+        mConstraintSet = new ConstraintSet();
+        mConstraintSet.clone(toolBarLayout);
+        toolBar.post(()-> header.post(()->{
+            int whiteSpace = toolBar.getWidth() - header.getWidth();
+            mConstraintSet.setMargin(header.getId(), ConstraintSet.END, whiteSpace/2);
+            mConstraintSet.applyTo(toolBarLayout);
+        }));
+
 
         foreWave = findViewById(R.id.foreWave);
         alphaGuide = findViewById(R.id.alphaGuide);
@@ -48,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(mNavController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(mNavController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
