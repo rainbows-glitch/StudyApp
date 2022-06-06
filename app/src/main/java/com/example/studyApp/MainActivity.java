1fc563d564393ae76e8ed94c4760a3001de4897b
Dashik <kumardashik@gmail.com>
2022-06-06 23:40:21 +1200

    added back button on nav slider disabled landscape and multitask modes

--- app/src/main/java/com/example/studyApp/MainActivity.java
+++ app/src/main/java/com/example/studyApp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.studyApp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -11,6 +12,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private ConstraintLayout toolBarLayout;
     private ConstraintSet mConstraintSet;
     private TextView header;
-    private ImageView foreWave;
-    private Guideline alphaGuide;
     private Toolbar toolBar;
     private NavController mNavController;
     private AppBarConfiguration mAppBarConfiguration;
+    private ImageView foreWave;
+    private Guideline alphaGuide;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +42,24 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navhost);
         assert navHost != null;
         mNavController = navHost.getNavController();
+        NavigationView drawer = findViewById(R.id.drawer);
 
         setSupportActionBar(toolBar);
 
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_study, R.id.nav_chat).setOpenableLayout(drawerLayout).build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.close_nav, R.id.nav_home, R.id.nav_study, R.id.nav_chat).setOpenableLayout(drawerLayout).build();
         NavigationUI.setupActionBarWithNavController(this, mNavController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController((NavigationView) findViewById(R.id.drawer), mNavController);
+        NavigationUI.setupWithNavController(drawer, mNavController);
+
+        drawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() != R.id.close_nav){
+                    NavigationUI.onNavDestinationSelected(item, mNavController);
+                }
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
 
         mConstraintSet = new ConstraintSet();
         mConstraintSet.clone(toolBarLayout);
@@ -54,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
             mConstraintSet.setMargin(header.getId(), ConstraintSet.END, whiteSpace/2);
             mConstraintSet.applyTo(toolBarLayout);
         }));
-
 
         foreWave = findViewById(R.id.foreWave);
         alphaGuide = findViewById(R.id.alphaGuide);
