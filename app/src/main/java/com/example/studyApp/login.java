3f86d66c9ee7f20e1ba29906c7896a98beb5a9e0
Dashik <kumardashik@gmail.com>
2022-06-09 23:19:32 +1200

    glassMorphism

---
+++ app/src/main/java/com/example/studyApp/login.java
@@ -0,0 +1,26 @@
+package com.example.studyApp;
+
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.media.Image;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class login extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return view;
+    }
+}
