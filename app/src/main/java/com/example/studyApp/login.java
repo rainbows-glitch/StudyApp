ece3ace05db27a2a4c265cf3c97d873d4e860df8
Dashik <kumardashik@gmail.com>
2022-06-11 16:44:59 +1200

    glassMorphism

--- app/src/main/java/com/example/studyApp/login.java
+++ app/src/main/java/com/example/studyApp/login.java
@@ -1,25 +1,42 @@
 package com.example.studyApp;
 
-import android.graphics.RenderEffect;
-import android.graphics.Shader;
-import android.media.Image;
-import android.os.Build;
 import android.os.Bundle;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
-
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 public class login extends Fragment {
+    private View glass;
+    private ConstraintLayout loginLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        glass = view.findViewById(R.id.glass);
+        loginLayout = view.findViewById(R.id.loginLayout);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+//        set glass height to be 85% of window height
+        float vh = displayMetrics.heightPixels/100;
+        glass.getLayoutParams().height = (int) vh*85;
+
+//        centre glass relative to window (not fragment)
+        ConstraintSet mConstraintSet = new ConstraintSet();
+        mConstraintSet.clone(loginLayout);
+        glass.post(()->{
+            int verticalSpace = displayMetrics.heightPixels - glass.getHeight();
+            mConstraintSet.setMargin(glass.getId(), ConstraintSet.BOTTOM, verticalSpace/2);
+            mConstraintSet.applyTo(loginLayout);
+            });
 
         return view;
     }
