77703c9b83df8075b6fbd2566a881a3ea96ebd92
Dashik <kumardashik@gmail.com>
2022-06-27 23:06:45 +1200

    almost finished home UI

--- app/src/main/java/com/example/studyApp/home.java
+++ app/src/main/java/com/example/studyApp/home.java
@@ -10,19 +10,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.OverScroller;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 
 public class home extends Fragment {
@@ -38,6 +39,9 @@ public class home extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
 //        show navigation since we know users might enter home from login/signup
         ((MainActivity)requireActivity()).showNavigation();
 
@@ -47,9 +51,9 @@ public class home extends Fragment {
         }else{
             int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
             String greeting;
-            if(hour >= 1 && hour < 12){
+            if(hour >= 0 && hour < 12){
                 greeting = "Good Morning";
-            }else if(hour >=11 && hour <18){
+            }else if(hour >=12 && hour <18){
                 greeting = "Good Afternoon";
             }else{
                 greeting = "Good Evening";
@@ -66,20 +70,27 @@ public class home extends Fragment {
             }
             welcomeName.setText(name);
 
+
             RecyclerView rv = view.findViewById(R.id.horizontalRecycle);
             sliderAdapter parentAdapter = new sliderAdapter(getContext());
-            rv.setAdapter(parentAdapter);
             LinearLayoutManager parentLayout = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+            rv.setAdapter(parentAdapter);
             rv.setLayoutManager(parentLayout);
+
             SnapHelper snapHelper = new LinearSnapHelper();
             snapHelper.attachToRecyclerView(rv);
-            rv.scrollToPosition(13);
+
+            int today = LocalDateTime.now().getDayOfWeek().getValue() -1;
+            TextView dayName = view.findViewById(R.id.day);
+            rv.scrollToPosition(10 + today);
+            updateDayName(10 + today, dayName);
 
 //            make buttons functional
             view.findViewById(R.id.backArrow).setOnClickListener(view1 -> {
                 if(parentLayout.findFirstVisibleItemPosition() >0){
-                    rv.smoothScrollToPosition(parentLayout.findFirstVisibleItemPosition() -1);
-                    updateDayName(parentLayout, view.findViewById(R.id.day));
+                    int pos = parentLayout.findFirstVisibleItemPosition() -1;
+                    rv.smoothScrollToPosition(pos);
+                    updateDayName(pos, dayName);
                     view.findViewById(R.id.frontArrow).setVisibility(View.VISIBLE);
                     if (parentLayout.findFirstVisibleItemPosition()-1 == 0){
                         view.findViewById(R.id.backArrow).setVisibility(View.GONE);
@@ -90,16 +101,35 @@ public class home extends Fragment {
                 }
             });
             view.findViewById(R.id.frontArrow).setOnClickListener(view1 -> {
-                if(parentLayout.findLastVisibleItemPosition()< 24){
-                    rv.smoothScrollToPosition(parentLayout.findLastVisibleItemPosition() +1);
-                    updateDayName(parentLayout, view.findViewById(R.id.day));
+                if (parentLayout.findLastVisibleItemPosition() < 24) {
+                    int pos = parentLayout.findLastVisibleItemPosition() + 1;
+                    rv.smoothScrollToPosition(pos);
+                    updateDayName(pos, dayName);
                     view.findViewById(R.id.backArrow).setVisibility(View.VISIBLE);
-                    if (parentLayout.findLastVisibleItemPosition()+1 == 24){
+                    if (parentLayout.findLastVisibleItemPosition() + 1 == 24) {
                         view.findViewById(R.id.frontArrow).setVisibility(View.GONE);
                     }
-                }else{
+                } else {
                     rv.smoothScrollToPosition(24);
                     view.findViewById(R.id.frontArrow).setVisibility(View.GONE);
+                }
+            });
+
+            rv.setOnFlingListener(new RecyclerView.OnFlingListener() {
+                @Override
+                public boolean onFling(int velocityX, int velocityY) {
+                    float dist = snapHelper.calculateScrollDistance(velocityX, velocityY)[0];
+                    float holderWidth = displayMetrics.widthPixels;
+                    int n = Math.round(dist/holderWidth);
+                    if(velocityX >0){
+                        int pos = parentLayout.findLastVisibleItemPosition() +n; //we don't need to plus or minus 1 to pos as swiping as already changed pos by 1
+                        updateDayName(pos, dayName);
+                        return false;
+                    }else if(velocityX <0){
+                        int pos = parentLayout.findFirstVisibleItemPosition() +n;
+                        updateDayName(pos, dayName);
+                        return false;
+                    }else {return false;}
                 }
             });
         }
@@ -107,11 +137,9 @@ public class home extends Fragment {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public void updateDayName(LinearLayoutManager layout, TextView textView){
-        int pos;
+    public void updateDayName(int position, TextView textView){
         String dayName;
-        pos = layout.findFirstVisibleItemPosition()%5 +1;
-        dayName = DayOfWeek.of(pos).toString();
+        dayName = DayOfWeek.of(position%5 +1).toString();
         dayName = dayName.charAt(0) + dayName.substring(1).toLowerCase();
         textView.setText(dayName);
     }
